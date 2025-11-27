e635b3898ee2e9071ac96b3752594379c30ac761
nishant <nishantraj2052004@gmail.com>
2025-11-27 23:06:34 +0530

    java bacis code

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,22 @@
+/*
+{
+    its called scope
+}   
+*/
+
+const accountId = 10000;
+let accountEmail = "nishant205200@gmail.com";
+var accountPassword = "123";
+country = "india";
+
+//  accountId = 2 not alllow
+
+accountEmail ="nishu@gmail.com";
+accountPassword ="567";
+country ="london";
+ 
+console.log([accountId,accountEmail,accountPassword ]);
+console.log(country);
+
+
+
