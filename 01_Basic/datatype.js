060f13e695f3f70023c8048b4ef8cc75d9e09c9b
nishant <nishantraj2052004@gmail.com>
2025-11-28 23:06:27 +0530

    fixed

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,23 @@
+
+let name = "nishant raj";
+let age = 19;
+let isLoggedIn = false;
+
+// number = 23
+
+// string = ""
+
+//boolean = true/false
+
+// null = standalone value
+
+// the type of null is object
+
+//undefined = 
+
+// type of undefined is undefined
+
+// symbol = unique
+
+
+// object
