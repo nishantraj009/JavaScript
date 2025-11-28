8865633d8a69ae5295bfcc04c933cfd53fe5fd1a
nishant <nishantraj2052004@gmail.com>
2025-11-29 00:53:00 +0530

    updated

--- 01_Basic/03_conversion.js
+++ 01_Basic/03_conversion.js
@@ -1,3 +1,17 @@
-let score = 33;
+
+/*
+let score = 33;        "33" it's a string type
+
 console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof (score));  both type is number
+*/
+
+ let score = "354abc";
+ console.log(typeof score);  // Both are string
+ console.log(typeof (score));
+
+ let convertInNum = Number(score);
+ console.log(typeof convertInNum);  // type is number
+ console.log(convertInNum); //NaN, not a number 
+
+  
