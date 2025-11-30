6e60766a4fcd79526e650a204be65e5d07f44c4f
nishant <nishantraj2052004@gmail.com>
2025-12-01 02:06:08 +0530

    fixed

---
+++ 01_Basic/05_strings.js
@@ -0,0 +1,8 @@
+ // string concatenation :- it means joining two or more string together .
+ 
+ const name = "Nishant";
+ const roll = 90;
+
+ console.log(name + " " + roll  +"section"); // this concatenation way right but not good.
+
+ console.log(`Hello, my name is ${name} and roll no is ${roll}`); // right way to join 2 or more string.
