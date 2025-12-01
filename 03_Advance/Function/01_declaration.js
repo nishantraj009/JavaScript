13ced353950d06d0ccf77dde435b61f19eb6f7a3
nishant <nishantraj2052004@gmail.com>
2025-12-02 01:36:32 +0530

    fun code fixed

---
+++ 03_Advance/Function/01_declaration.js
@@ -0,0 +1,22 @@
+
+// Term	Meaning
+
+// Function Declaration	Function banana
+
+// Function Call :	Function ko chalana
+
+// Parameters :	Function me input lene wale variables
+
+// Return :	Result wapas bhejna
+
+
+
+function sayHello() { // its function declaration
+    console.log("Hello JavaScript!");
+}
+
+sayHello(); // Function call
+
+
+
+// output : Hello JavaScript!
---
+++ 03_Advance/Function/02_parameter.js
@@ -0,0 +1,22 @@
+
+
+// What is Parameter & Argument:
+
+// Term	Meaning
+
+// Parameter:   Function define karte waqt jo variables dete hain
+
+// Argument :	Function call karte waqt jo actual values dete hain
+
+
+
+
+function greet(name) {   // name is a parameter
+    console.log("Hello " + name);
+}
+
+greet("Nishant"); // "Nishant" is an argument
+
+
+// output : Hello Nishant
+
