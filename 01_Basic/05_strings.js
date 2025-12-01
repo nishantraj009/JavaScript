6a7e9c5a5da5a2f6a40b676c04f2de6f4be3ab98
nishant <nishantraj2052004@gmail.com>
2025-12-01 15:14:36 +0530

    fixed

--- 01_Basic/05_strings.js
+++ 01_Basic/05_strings.js
@@ -1,8 +1,14 @@
  // string concatenation :- it means joining two or more string together .
  
- const name = "Nishant";
+ const name = "NishantRaj"; // string declaration
  const roll = 90;
 
  console.log(name + " " + roll  +"section"); // this concatenation way right but not good.
 
  console.log(`Hello, my name is ${name} and roll no is ${roll}`); // right way to join 2 or more string.
+
+
+
+ // string declaration 2 nd way
+
+ const naam = new String("nishant");   // this declaration give length property and methods.
