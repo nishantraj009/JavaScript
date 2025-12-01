8f574662723fc185ace2becf42887a2df844a3b4
nishant <nishantraj2052004@gmail.com>
2025-12-02 01:09:07 +0530

    fixed code

---
+++ 02_Basic/03_loop.js
@@ -0,0 +1,21 @@
+// Print numbers from 1 to 10 using for loop
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Print numbers from 1 to 10 using while loop
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;  // increment karna mat bhoolna
+}
+
+
+// Print numbers from 1 to 10 using do-while loop
+let j = 1;
+
+do {
+    console.log(j);
+    j++;  // pehle run, baad me condition check
+} while (j <= 10);
