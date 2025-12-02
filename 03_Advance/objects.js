f2a22132477ccda097b416ad30e2a539662d1b24
nishant <nishantraj2052004@gmail.com>
2025-12-03 00:54:49 +0530

    fixed

--- 03_Advance/objects.js
+++ 03_Advance/objects.js
@@ -1,5 +1,18 @@
 
-// OBJECT LITERALS
+// JS OBJECT LITERALS :
+
+// objects are a collection of property
+// property = (key,value) pairs
+
+// eg :     student
+        //   ⬇️
+     //  name : "nishant" (its property)
+
+
+// use to store keyed collection & complex entities
+
+
+
 
 // const student = {} , this is object , empaty object
 
