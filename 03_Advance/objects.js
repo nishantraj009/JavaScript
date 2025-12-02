7513bfc442e6e46ad2215e264de90cc37ff7406b
nishant <nishantraj2052004@gmail.com>
2025-12-03 00:00:21 +0530

    update

---
+++ 03_Advance/objects.js
@@ -0,0 +1,13 @@
+
+// OBJECT LITERALS
+
+// const student = {}  ,// this is object , empaty object
+
+const user = {
+    name: "nishant";
+    age:20;
+    location: meerut;
+
+}
+
+console.log(user.name);
